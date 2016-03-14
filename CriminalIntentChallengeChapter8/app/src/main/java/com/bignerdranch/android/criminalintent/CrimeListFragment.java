4dcdaab345390a3bafbb6cbd6153ca6701924481
Kyle Roe <kyle.d.roe@gmail.com>
2016-03-14 20:36:31 +0100

    Challenge Chapter 8

---
+++ CriminalIntentChallengeChapter8/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -0,0 +1,9 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by kyle on 1/19/16.
+ */
+public class CrimeListFragment extends Fragment {
+}
---
+++ CriminalIntentChallengeChapter8/app/src/main/java/com/bignerdranch/android/criminalintent/SingleFragmentActivity.java
@@ -0,0 +1,30 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+
+/**
+ * Created by kyle on 1/19/16.
+ */
+public abstract class SingleFragmentActivity extends FragmentActivity {
+
+    protected abstract Fragment createFragment();
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragment = fragmentManager.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = createFragment();
+            fragmentManager.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
+    }
+}
