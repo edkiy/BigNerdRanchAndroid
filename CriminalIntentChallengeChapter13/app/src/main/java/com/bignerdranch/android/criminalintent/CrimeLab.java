ce13aa89cc3637b634a6e298b1c41e0820ea57ef
Kyle <hopeman1986@gmail.com>
2016-05-30 16:59:01 +0200

    Challenge Chapter 13

---
+++ CriminalIntentChallengeChapter13/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
@@ -0,0 +1,48 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Created by kyle.roe on 23/02/16.
+ */
+public class CrimeLab {
+
+    private List<Crime> mCrimes;
+    private static CrimeLab sCrimeLab;
+
+    public static CrimeLab get(Context context) {
+        if (sCrimeLab == null) {
+            sCrimeLab = new CrimeLab(context);
+        }
+        return sCrimeLab;
+    }
+
+    private CrimeLab(Context context) {
+        mCrimes = new ArrayList<>();
+    }
+
+    public void addCrime(Crime crime) {
+        mCrimes.add(crime);
+    }
+
+    public void deleteCrime(Crime crime) {
+        mCrimes.remove(crime);
+    }
+
+    public List<Crime> getCrimes() {
+        return mCrimes;
+    }
+
+    public Crime getCrime(UUID id) {
+        for (Crime crime : mCrimes) {
+            if (crime.getId().equals(id)) {
+                return crime;
+            }
+        }
+        return null;
+    }
+}
