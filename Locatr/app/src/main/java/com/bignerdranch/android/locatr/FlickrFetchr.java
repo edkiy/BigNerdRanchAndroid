0f4a07c30539036e6031f71282898f5d6d537b3d
Roe <kyle.roe@apertomove.de>
2016-10-24 13:56:43 +0200

    End of chapter 31

---
+++ Locatr/app/src/main/java/com/bignerdranch/android/locatr/FlickrFetchr.java
@@ -0,0 +1,151 @@
+package com.bignerdranch.android.locatr;
+
+import android.content.Context;
+import android.location.Location;
+import android.net.Uri;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.net.ssl.HttpsURLConnection;
+
+/**
+ * Created by kyle.roe on 10/10/16.
+ */
+
+public class FlickrFetchr {
+
+    private static final String TAG = "FlickrFetchr";
+    private static final String FETCH_RECENT_METHOD = "flickr.photos.getRecent";
+    private static final String SEARCH_METHOD = "flickr.photos.search";
+
+    public byte[] getUrlBytes(String urlSpec) throws IOException {
+        URL url = new URL(urlSpec);
+        HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+
+        try {
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            InputStream in = connection.getInputStream();
+
+            if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
+                throw new IOException(connection.getResponseMessage() +
+                        ": with " +
+                        urlSpec);
+            }
+
+            int bytesRead = 0;
+            byte[] buffer = new byte[1024];
+            while ((bytesRead = in.read(buffer)) > 0) {
+                out.write(buffer, 0, bytesRead);
+            }
+            out.close();
+            return out.toByteArray();
+        } finally {
+            connection.disconnect();
+        }
+    }
+
+    public String getUrlString(String urlSpec) throws IOException {
+        return new String(getUrlBytes(urlSpec));
+    }
+
+    public List<GalleryItem> fetchRecentPhotos(Context context) {
+        String url = buildUrl(context, FETCH_RECENT_METHOD, null);
+        return downloadGalleryItems(url);
+    }
+
+    public List<GalleryItem> searchPhotos(Context context, String query) {
+        String url = buildUrl(context, SEARCH_METHOD, query);
+        return downloadGalleryItems(url);
+    }
+
+    public List<GalleryItem> searchPhotos(Context context, Location location) {
+        String url = buildUrl(context, location);
+        return downloadGalleryItems(url);
+    }
+
+    private List<GalleryItem> downloadGalleryItems(String url) {
+        List<GalleryItem> items = new ArrayList<>();
+
+        try {
+            String jsonString = getUrlString(url);
+            Log.i(TAG, "Received JSON: " + jsonString);
+
+            JSONObject jsonBody = new JSONObject(jsonString);
+            parseItems(items, jsonBody);
+        } catch (JSONException e) {
+            Log.e(TAG, "Failed to parse JSON" + e);
+        } catch (IOException e) {
+            Log.e(TAG, "Failed to fetch items", e);
+        }
+
+        return items;
+    }
+
+    private String buildUrl(Context context, String method, String query) {
+        Uri endpoint = Uri.parse("https://api.flickr.com/services/rest/")
+                .buildUpon()
+                .appendQueryParameter("api_key", context.getString(R.string.flickr_api_key))
+                .appendQueryParameter("format", "json")
+                .appendQueryParameter("nojsoncallback", "1")
+                .appendQueryParameter("extras", "url_s")
+                .build();
+
+        Uri.Builder uriBuilder = endpoint.buildUpon()
+                .appendQueryParameter("method", method);
+
+        if (method.equals(SEARCH_METHOD)) {
+            uriBuilder.appendQueryParameter("text", query);
+        }
+
+        Log.d(TAG, "Url: " + uriBuilder.build().toString());
+
+        return uriBuilder.build().toString();
+    }
+
+    private String buildUrl(Context context, Location location) {
+        Uri endpoint = Uri.parse("https://api.flickr.com/services/rest/")
+                .buildUpon()
+                .appendQueryParameter("api_key", context.getString(R.string.flickr_api_key))
+                .appendQueryParameter("format", "json")
+                .appendQueryParameter("nojsoncallback", "1")
+                .appendQueryParameter("extras", "url_s")
+                .appendQueryParameter("method", SEARCH_METHOD)
+                .appendQueryParameter("lat", "" + location.getLatitude())
+                .appendQueryParameter("lon", "" + location.getLongitude())
+                .build();
+
+        return endpoint.toString();
+    }
+
+    private void parseItems(List<GalleryItem> items, JSONObject jsonBody) throws IOException, JSONException {
+        JSONObject photosJsonObject = jsonBody.getJSONObject("photos");
+        JSONArray photoJsonArray = photosJsonObject.getJSONArray("photo");
+
+        for (int i = 0; i < photoJsonArray.length(); i++) {
+            JSONObject photoJsonObject = photoJsonArray.getJSONObject(i);
+
+            GalleryItem item = new GalleryItem();
+            item.setId(photoJsonObject.getString("id"));
+            item.setCaption(photoJsonObject.getString("title"));
+
+            if (!photoJsonObject.has("url_s")) {
+                continue;
+            }
+
+            item.setUrl(photoJsonObject.getString("url_s"));
+            items.add(item);
+        }
+        Log.i(TAG, "Item size: " + items.size());
+    }
+}
